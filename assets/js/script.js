8b6ceeb689e8883e5bbbc8f0280c8f5b73fcd8e4
vishnududi-511 <dudivishnu4@gmail.com>
2024-03-19 12:24:37 +0530

    navbar-responsive and sec-1 static completed

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let menubox = document.querySelector(".menubox");
+let navitems = document.querySelector(".navitems");
+ 
+menubox.addEventListener("click", function () {
+    navitems.classList.toggle("active")
+})
